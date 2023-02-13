acea950a551e1f4a8d96411c65a28539d107a768
JarekKrolik <madmax@onet.eu>
2023-02-13 21:01:30 +0100

    added methods to search movies and actor by description

--- repository/MovieFinder.ts
+++ repository/MovieFinder.ts
@@ -1,5 +1,6 @@
 import {MovieEntity} from "../types";
 import fetch from 'cross-fetch';
+import {imdbApiKey} from "../utils/config/apiKeyConfig";
 
 export class MovieFinder  implements MovieEntity {
 
@@ -10,7 +11,7 @@ export class MovieFinder  implements MovieEntity {
     title: string;
 
     static async getAllByTitle (title:string,lang='en'):Promise<MovieEntity[]>{
-        const res = await fetch(`https://imdb-api.com/${lang}/API/SearchMovie/k_9gtvw9rs/${title}`);
+        const res = await fetch(`https://imdb-api.com/${lang}/API/SearchMovie/${imdbApiKey}/${title}`);
         const data = await res.json() as MovieEntity[];
 
 
@@ -20,21 +21,65 @@ export class MovieFinder  implements MovieEntity {
         return data;
 };
 
-    static async getComingSoonMovies():Promise<[]>{
-        const res = await fetch(`https://imdb-api.com/en/API/ComingSoon/k_9gtvw9rs`);
+    static async getAllSeriesByTitle (title:string,lang='en'):Promise<MovieEntity[]>{
+        const res = await fetch(`https://imdb-api.com/${lang}/API/SearchSeries/${imdbApiKey}/${title}`);
+        const data = await res.json() as MovieEntity[];
+
+
+        // console.log(data)
+
+
+        return data;
+    };
+
+    static async getComingSoonMovies(lang='en'):Promise<any[]>{
+        const res = await fetch(`https://imdb-api.com/${lang}/API/ComingSoon/${imdbApiKey}`);
         const data = await res.json() ;
 
-        console.log(data)
+        // console.log(data)
         return data;
     }
 
-    static async nowInCinemas():Promise<[]>{
-        const res = await fetch(`https://imdb-api.com/en/API/InTheaters/k_9gtvw9rs`);
+    static async nowInCinemas(lang='en'):Promise<any[]>{
+        const res = await fetch(`https://imdb-api.com/${lang}/API/InTheaters/${imdbApiKey}`);
         const data = await res.json() ;
 
-        console.log(data)
+        // console.log(data)
         return data;
     }
+
+static async getOneMovieById(id:string,lang='en'):Promise<MovieEntity>{
+    const res = await fetch(`https://imdb-api.com/${lang}/API/Title/${imdbApiKey}/${id}/FullActor, FullCast, Posters, Images, Trailer, Ratings, Wikipedia`);
+    const data = await res.json() as MovieEntity
+
+
+    console.log(data)
+
+
+        return data;
+}
+
+
+
+
+    static async findActorByName(name:string,lang='en'):Promise<any[]>{
+        const res = await fetch(`https://imdb-api.com/en/API/SearchName/${imdbApiKey}/${name}`)  ;
+        const data = await res.json();
+
+        console.log(data)
+        return data
+    }
+
+    static async findActorById(id:string,lang='en'):Promise<any[]>{
+        const res = await fetch(`https://imdb-api.com/${lang}/API/Name/${imdbApiKey}/${id}`)  ;
+        const data = await res.json();
+
+        console.log(data)
+        return data
+    }
+
+
+
 
 
 }
