7e981a6d5023b77ba1f93ada09b163134bef68ba
JarekKrolik <madmax@onet.eu>
2023-03-01 19:30:45 +0100

    added new types in singleMovieSpecific, added new endpoints in favourite router

--- routers/favouriteRouter.ts
+++ routers/favouriteRouter.ts
@@ -1,50 +1,67 @@
 import {Request, Response, Router} from "express";
 import {pool} from "../utils/db";
-import {MovieListEntity} from "../types";
+import {ActorsListEntity, MovieListEntity} from "../types";
 import {FieldPacket} from "mysql2";
 
 
 export const favouriteRouter = Router()
-    .get('/:user',async (req:Request,res:Response)=>{
+    .get('/actor/:user', async (req: Request, res: Response) => {
         const user = req.params.user
-        const data = await pool.execute("SELECT * FROM `favourites` WHERE `user`=:user",{
-            user:user,
-        }) as [MovieListEntity[],FieldPacket[]]
-
+        const data = await pool.execute("SELECT * FROM `favourite_actors` WHERE `user`=:user", {
+            user: user,
+        }) as [ActorsListEntity[], FieldPacket[]]
         res.json(data[0])
-        console.log(data[0])
-
 
     })
-    .post('/',async (req:Request,res:Response)=>{
+    .get('/:user', async (req: Request, res: Response) => {
+        const user = req.params.user
+        const data = await pool.execute("SELECT * FROM `favourites` WHERE `user`=:user", {
+            user: user,
+        }) as [MovieListEntity[], FieldPacket[]]
+        res.json(data[0])
+
+    })
+    .post('/', async (req: Request, res: Response) => {
         const data = await req.body;
 
 
-if(data.type==='movie'){
-    await pool.execute("INSERT INTO `favourites` (`movie_id`,`user`) VALUES (:id,:user)",{
-        id:data.id,
-        user:data.user,
-
+        if (data.type === 'movie') {
+            await pool.execute("INSERT INTO `favourites` (`movie_id`,`user`,`name`,`image`) VALUES (:id,:user,:name,:image)", {
+                id: data.id,
+                user: data.user,
+                name: data.title,
+                image: data.image,
+            })
+        }
+        if (data.type === 'actor') {
+            await pool.execute("INSERT INTO `favourite_actors` (`actor_id`,`user`,`name`,`image`) VALUES (:id,:user,:name,:image)", {
+                id: data.id,
+                user: data.user,
+                name: data.title,
+                image: data.image,
+            })
+        }
 
     })
-}
-    })
-    .delete('/',async (req:Request,res:Response)=>{
-        try{
-        const data =await req.body;
-        if(data.type==='movie'){
-          await pool.execute(" DELETE FROM `favourites` WHERE `movie_id`=:id AND `user`=:user",{
-              id:data.id,
-              user:data.user,
-          })
-        }}catch (e) {
+    .delete('/', async (req: Request, res: Response) => {
+        try {
+            const data = await req.body;
+            if (data.type === 'movie') {
+                await pool.execute(" DELETE FROM `favourites` WHERE `movie_id`=:id AND `user`=:user", {
+                    id: data.id,
+                    user: data.user,
+                })
+            }
+            if(data.type==='actor'){
+                await pool.execute(" DELETE FROM `favourite_actors` WHERE `actor_id`=:id AND `user`=:user", {
+                    id: data.id,
+                    user: data.user,
+                })
+            }
+        } catch (e) {
             res.json(e)
             throw new Error(e)
         }
-
-
-
-
 
 
     })
--- types/movieTypes/movie.maniac.types.ts
+++ types/movieTypes/movie.maniac.types.ts
@@ -1,114 +1,139 @@
 export interface MovieListEntity {
-    id:string,
-    title:string,
-    description:string,
-    image:string,
-    resultType:string,
-
+    id: string,
+    title: string,
+    description: string,
+    image: string,
+    resultType: string,
+    errorMessage: string,
 }
 
+export interface Response {
+    "searchType": string,
+    "expression": string,
+    "results": MovieListEntity[] | ActorsListEntity[]
+    "errorMessage": string,
+}
 
-export interface NowInCinemasMovieEntity extends MovieListEntity{
+export interface NowInCinemasMovieEntity extends MovieListEntity {
 
-    fullTitle:string,
-    genres:string,
-    year:string,
-    stars:string,
-    releaseState:string,
-
+    fullTitle: string,
+    genres: string,
+    year: string,
+    stars: string,
+    releaseState: string,
+    errorMessage: string,
 
 
 }
 
 export interface SingleMovieSpecific {
-    errorMessage:string,
-    fullCast:{
-        actors:{
-asCharacter:string,
+    posters: {
+        backdrops: {
             id:string,
-            image:string,
-            name:string,
+            link:string,
+        }[],
+        posters: {
+            id: string,
+            link: string,
+        }[]
+    }
+    errorMessage: string,
+    fullCast: {
+        actors: {
+            asCharacter: string,
+            id: string,
+            image: string,
+            name: string,
         }[]
     },
-    awards:string,
-    trailer:{
-        link:string,
-        linkEmbed:string,
+    awards: string,
+    trailer: {
+        link: string,
+        linkEmbed: string,
     },
-    countries:string,
-    companies:string,
-    contentRating:string,
-    directors:string,
-    fullTitle:string,
-    genres:string,
-    imDbRating:string,
-    image:string,
-    ratings:{
-        filmAffinity:string,
-        imDb:string,
-        metacritic:string,
-        rottenTomatoes:string,
-        theMovieDb:string,
+    countries: string,
+    companies: string,
+    contentRating: string,
+    directors: string,
+    fullTitle: string,
+    genres: string,
+    imDbRating: string,
+    image: string,
+    ratings: {
+        filmAffinity: string,
+        imDb: string,
+        metacritic: string,
+        rottenTomatoes: string,
+        theMovieDb: string,
 
     },
-    images:{
-        items:{
-            image:string,
-            title:string,
+    images: {
+        items: {
+            image: string,
+            title: string,
         }[],
     };
-    keywords:string,
-    plot:string,
-    metacriticRating:string,
-    releaseDate:string,
-    stars:string,
-    tagline:string|null,
-    year:string,
-    languages:string,
-    writers:string,
-    wikipedia:object,
-    type:string,
-    runtimeStr:string,
-boxOffice:{
-        budget:string,
-    cumulativeWorldwideGross:string,
+    keywords: string,
+    plot: string,
+    metacriticRating: string,
+    releaseDate: string,
+    stars: string,
+    tagline: string | null,
+    year: string,
+    languages: string,
+    writers: string,
+    wikipedia: object,
+    type: string,
+    runtimeStr: string,
+    boxOffice: {
+        budget: string,
+        cumulativeWorldwideGross: string,
 
-}
+    }
 
 }
 
 export interface ActorsListEntity {
-    description:string,
-    id:string,
-    image:string,
-    resultType:string,
-    title:string,
+    description: string,
+    id: string,
+    image: string,
+    resultType: string,
+    title: string,
+    errorMessage: string,
 
 }
 
 export interface SingleActorSpecific {
-    errorMessage:string,
-    id:string,
-    name:string,
-    image:string,
-    summary:string,
-    awards:string,
-    birthDate:string,
-    deathDate:string,
-    height:string,
-    castMovies:{
-        description:string,
-        id:string,
-        role:string,
-        title:string,
-        year:string,
+    errorMessage: string,
+    id: string,
+    name: string,
+    image: string,
+    summary: string,
+    awards: string,
+    birthDate: string,
+    deathDate: string,
+    height: string,
+    castMovies: {
+        description: string,
+        id: string,
+        role: string,
+        title: string,
+        year: string,
     }[],
-
 
 
 }
 
 export interface FavouriteMoviesList {
-    user:string,
-    movie_id:string,
+    user: string,
+    movie_id: string,
+    name: string,
+    image: string,
+}
+
+export interface FavouriteActorsList {
+    user: string,
+    actor_id: string,
+    name: string,
+    image: string,
 }
