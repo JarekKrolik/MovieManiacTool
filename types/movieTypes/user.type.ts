9da17b36119e4cf1ee8370ae12919d378f11776e
JarekKrolik <madmax@onet.eu>
2023-03-14 07:48:46 +0100

    added user data interface in types folder

--- types/movieTypes/user.type.ts
+++ types/movieTypes/user.type.ts
@@ -1,13 +1,50 @@
-
 export interface UserEntity {
-    id?:string,
-    name:string,
-    email:string,
-    passwordhash:string,
-    isverified:boolean,
-    avatar:number,
-    verification_code?:number|null,
-    date?:any,
+    id?: string,
+    name: string,
+    email: string,
+    passwordhash: string,
+    isverified: boolean,
+    avatar: number,
+    verification_code?: number | null,
+    date?: any,
+
+
+}
+
+export interface UserData {
+
+    id: string,
+    name: string,
+    date: string,
+    avatar: number,
+    email: string,
+    movieId: string,
+    favMovies: [{
+        movie_id: string,
+        user: string,
+        name: string,
+        image: string,
+    }],
+    favActors: [{
+        actor_id: string,
+        user: string,
+        name: string,
+        image: string,
+    }],
+    searchList: [
+        {
+            id: string,
+            title: string,
+            description: string,
+            image: string,
+            resultType: string,
+            errorMessage: string,
+        },
+    ],
+    selectedItem: {
+        id: string,
+        type: string,
+    }
 
 
 }
